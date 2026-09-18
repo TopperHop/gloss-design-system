9a49e7c67d7bea1c0efb891d344e70494526a093
Mike Rockwood <3772632+mrockwood@users.noreply.github.com>
2026-09-18 17:43:39 -0600

    Align tooling with Node 24 and SCSS

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('stylelint').Config} */
 export default {
-    extends: ['stylelint-config-standard'],
+    extends: ['stylelint-config-standard-scss'],
     ignoreFiles: ['node_modules/**', 'dist/**', '_site/**'],
 };
